330103d00d7569c4edce0ab95134439a7c64d42f
Ricky-yu <ts00484515@gmail.com>
2020-10-22 21:00:11 +0900

    setting up

---
+++ MusicPlay/MusicPlay/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MusicPlay
+//
+//  Created by CHEN SINYU on 2020/10/22.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
