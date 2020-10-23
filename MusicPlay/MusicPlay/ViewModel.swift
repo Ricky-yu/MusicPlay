5192650005c4bdf375f64bf565bdd9922e723f64
Ricky-yu <ts00484515@gmail.com>
2020-10-23 23:05:22 +0900

    add ViewModel

---
+++ MusicPlay/MusicPlay/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  MusicPlay
+//
+//  Created by CHEN SINYU on 2020/10/23.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
