252567342c9b604fc0a4a4b8b9d2299a3548e36c
Ricky-yu <ts00484515@gmail.com>
2020-10-23 23:00:33 +0900

    add Model and update AutoLayoout

---
+++ MusicPlay/MusicPlay/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  MusicPlay
+//
+//  Created by CHEN SINYU on 2020/10/23.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import RxSwift
+
+final class Model: NSObject {
+
+}
