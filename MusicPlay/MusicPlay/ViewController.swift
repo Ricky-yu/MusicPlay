377e02b2085317f28664a0ae1bd2f85436d5f73e
Ricky-yu <ts00484515@gmail.com>
2020-10-26 23:26:41 +0900

    Musicのplay and stop 実装　後でMVVM分ける

--- MusicPlay/MusicPlay/ViewController.swift
+++ MusicPlay/MusicPlay/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MediaPlayer
+import MarqueeLabel
 import RxSwift
 import RxCocoa
 
@@ -14,7 +16,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var songImageView: UIImageView!
     @IBOutlet weak var timeSlider: UISlider!
-    @IBOutlet weak var songName: UILabel!
+    @IBOutlet weak var songName: MarqueeLabel!
     @IBOutlet weak var albumName: UILabel!
     @IBOutlet weak var currentSongTime: UILabel!
     @IBOutlet weak var totalSongTime: UILabel!
@@ -22,12 +24,111 @@ class ViewController: UIViewController {
     @IBOutlet weak var playBtn: UIButton!
     @IBOutlet weak var forwardBtn: UIButton!
     @IBOutlet weak var musicListBtn: UIButton!
-    
+    let disposeBag = DisposeBag();
+    let player = MPMusicPlayerController.applicationMusicPlayer
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        self.musicListBtn.rx.tap.bind{ [weak self] in
+            self?.checkPermitUseMusic()
+        }.disposed(by: disposeBag)
+        self.playBtn.rx.tap.bind{ [weak self] in
+            if self?.player.playbackState != .playing {
+                self?.player.play()
+                self?.playBtn.rx.backgroundImage().onNext(UIImage(named: "pauseIcon"))
+            } else {
+                self?.player.stop()
+                self?.playBtn.rx.backgroundImage().onNext(UIImage(named: "playIcon"))
+            }
+        }.disposed(by: disposeBag)
+        
     }
+    
+    func setup() {
+        self.songName.tag = 101
+        self.songName.type = .continuous
+        self.songName.animationCurve = .easeInOut
+        self.songName.speed = .duration(8.0)
+    }
+}
 
+extension ViewController: MPMediaPickerControllerDelegate {
 
+    func checkPermitUseMusic(){
+        let status = MPMediaLibrary.authorizationStatus()
+        switch status {
+        case .authorized:
+            let picker = MPMediaPickerController(mediaTypes: MPMediaType.music)
+            picker.delegate = self
+            picker.allowsPickingMultipleItems = false
+            picker.showsItemsWithProtectedAssets = false
+            picker.showsCloudItems = false
+            self.present(picker, animated: true, completion: nil)
+        case .notDetermined:
+            MPMediaLibrary.requestAuthorization() { status in
+                if status == .authorized {
+                    self.displayMPMediaPickerController()
+                }
+            }
+        default: self.displayPermissionAlertFromViewController()
+        }
+    }
+    
+    func displayMPMediaPickerController(){
+        let picker = MPMediaPickerController(mediaTypes: MPMediaType.music)
+        picker.delegate = self
+        picker.allowsPickingMultipleItems = true
+        picker.showsItemsWithProtectedAssets = false
+        picker.showsCloudItems = false
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func displayPermissionAlertFromViewController() {
+        let alert = UIAlertController(title: "メディアライブラリのアクセス許可を設定しますか？", message: "音楽選択するため", preferredStyle:  UIAlertController.Style.alert)
+        
+        alert.popoverPresentationController?.sourceView = self.view
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let okAction = UIAlertAction(title: "設定します", style: .default) { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alert.addAction(okAction)
+        
+        self.presentedViewController?.dismiss(animated: false, completion: nil)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func mediaPicker(_ mediaPicker: MPMediaPickerController,
+                     didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        player.stop()
+        player.setQueue(with: mediaItemCollection)
+        player.repeatMode = .all
+        
+        if let mediaItem = mediaItemCollection.items.first {
+            updateSongInformationUI(mediaItem : mediaItem)
+        }
+        player.play()
+        playBtn.setBackgroundImage(UIImage(named: "pauseIcon"), for: .normal)
+        self.dismiss(animated: true, completion: nil)
+       
+    }
+    
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func updateSongInformationUI(mediaItem: MPMediaItem) {
+        songName.text = mediaItem.title ?? "不明な曲"
+        albumName.text = mediaItem.albumTitle ?? "不明なアルバム"
+        if let artwork = mediaItem.artwork {
+            let image = artwork.image(at: songImageView.bounds.size)
+            songImageView.image = image
+        }
+    }
+    
 }
 
